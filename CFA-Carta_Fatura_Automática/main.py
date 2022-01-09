54e5f00efa18a171591af62788c89e7de782e459
Leandro Fratel de Souza <63375592+leandrofratel@users.noreply.github.com>
2022-01-09 14:40:07 -0300

    09/01

--- CFA-Carta_Fatura_Automática/main.py
+++ CFA-Carta_Fatura_Automática/main.py
@@ -1,15 +1,31 @@
 from docxtpl import DocxTemplate
 import pandas as pd
+from time import sleep
 
-#! Importação do Template e da Planilha.
+#? Importação do Template e da Planilha.
 doc = DocxTemplate('CFA-Carta_Fatura_Automática\Entrada\Template.docx')
-base = pd.read_excel('CFA-Carta_Fatura_Automática\Entrada\Base.xlsx')
+base = pd.read_excel('CFA-Carta_Fatura_Automática\Entrada\Recibo_fatura-backoffice.xls')
 
-data = base.to_dict(orient='list')
-for linha in data:
-    context = data
+#? Inicia o Laço que irá percorrer cada linha do Data Frame.
+def Carta_Automatica():
+    index = 0
+    for i in range(len(base)):
+        primeira_linha = base.iloc[index]
+        index = index + 1
+        context = dict(primeira_linha)
 
-#! Renderiza e salva o Doc.
-doc.render(context)
-doc.save('CFA-Carta_Fatura_Automática\Saida\Carta_Fatura_Pronta.docx')
-print('Arquivo criado com sucesso!')
+        #? Extrai o dado de cada linha para ser usado no nome do novo arquivo.
+        numero_SOE = primeira_linha['ID']
+
+        nome_do_arquivo = f'Carta_Fatura ID {numero_SOE}.docx'
+
+        #? Renderiza e salva o Doc.
+        doc.render(context)
+        doc.save(f'CFA-Carta_Fatura_Automática\Saida\ {nome_do_arquivo}')
+        print(f'Arquivo {numero_SOE} criado com sucesso!')
+        sleep(1)
+
+def Recibo_Automatico():
+    pass
+
+Carta_Automatica()
